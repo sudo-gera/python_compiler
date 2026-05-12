8dfb1dfac3fafa103117adc1a15f6503c466d86a
sudo-gera <gera.tatarinov.999@gmail.com>
2026-05-12 22:36:04 +0300

    *interesting commit message*

--- test_all.py
+++ test_all.py
@@ -22,7 +22,8 @@ walked_files = [*walk(
     os.path.dirname(os.path.realpath(__file__))
 )]
 
-# walked_files = ['test.py']
+if os.path.exists('last_failed.py'):
+    walked_files = ['last_failed.py']
 
 with open('test_seed.txt') as file:
     test_random = random.Random(file.read())
@@ -38,9 +39,11 @@ def test_ast(filepath):
         try:
             lib_ast = ast.parse(text, filepath)
             lib_dump = ast.dump(lib_ast)
+            lib_dump4 = ast.dump(lib_ast, indent=4)
         except SyntaxError:
             lib_ast = None
             lib_dump = None
+            lib_dump4 = None
 
         if lib_ast is not None and re.search(r'\bMatch\b', lib_dump):
             return
@@ -48,17 +51,21 @@ def test_ast(filepath):
         try:
             app_ast = create_ast.create_ast(text, filepath)
             app_dump = dump_ast.dump_ast(app_ast)
+            app_dump4 = dump_ast.dump_ast(app_ast, indent=4)
         except Exception:
             app_ast = None
             app_dump = None
+            app_dump4 = None
 
         assert app_dump == lib_dump
-
+        assert app_dump4 == lib_dump4
         assert create_ast.ast_equal(app_ast, lib_ast)
-
         assert create_ast.all_have_tokens(app_ast)
 
     except Exception:
-        with open('test.py', 'w') as wfile:
+        with open('last_failed.py', 'w') as wfile:
             wfile.write(text)
         raise
+
+def test_actual_tests_were_used():
+    assert walked_files != ['last_failed.py']
